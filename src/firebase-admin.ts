57084ea8ba6ebe49c16d71c42b8b116156081845
Kenneth Chan <hello@yeastoneast.com>
2025-12-05 14:14:26 +0000

    Try fixing this error: `Console Error: Service account must be an object

--- src/firebase-admin.ts
+++ src/firebase-admin.ts
@@ -1,17 +1,21 @@
 
 import * as admin from 'firebase-admin';
 
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
-  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-  : undefined;
+let adminDb: admin.firestore.Firestore;
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    // Other admin config if needed
-  });
+try {
+  const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+    ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+    : undefined;
+
+  if (serviceAccount && !admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+    adminDb = admin.firestore();
+  }
+} catch (error) {
+  console.error('Firebase Admin initialization error:', error);
 }
-
-const adminDb = admin.firestore();
 
 export { adminDb };
